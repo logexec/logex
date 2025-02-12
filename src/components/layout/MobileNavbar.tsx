fef8d1af066ee40ac2bbda22724154c19105bf76
rick-dev-logex <ricardo.estrella@logex.ec>
2025-02-12 17:41:06 -0500

    removed unused import

--- src/components/layout/MobileNavbar.tsx
+++ src/components/layout/MobileNavbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "motion/react";
 import { Code, Contact, Home, PackageSearch, Truck } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
