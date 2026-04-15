08766d4774a455f7f6f33c81877a34ce0eabfaba
Ricardo Estrella <ricardo.estrella@logex.ec>
2026-04-15 16:13:38 -0500

    feat: Added proxy to listen to API in localhost

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_APPOINTMENT_AVAILABILITY_API_URL?: string;
   readonly VITE_APPOINTMENTS_API_URL?: string;
   readonly VITE_CONTACT_API_URL?: string;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        target: "http://127.0.0.1:8001",
+      },
+    },
+  },
 });
